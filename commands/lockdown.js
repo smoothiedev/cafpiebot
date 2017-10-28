7e4c7487c34f6f94ec6bc4c20479ddb71d648b42
smoothiedev <yeochenxi@yahoo.com>
2017-10-28 23:12:35 +0800

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -2,6 +2,7 @@ const ms = require('ms');
 exports.run = (client, message, args) => {
   if (!client.lockit) client.lockit = [];
   const time = args.join(' ');
+  const modlog = client.channels.find('name', 'mod-log');
   const validUnlocks = ['release', 'unlock'];
   if (!time) return message.reply('You must set a duration for the lockdown in either hours, minutes or seconds');
 
@@ -9,7 +10,24 @@ exports.run = (client, message, args) => {
     message.channel.overwritePermissions(message.guild.id, {
       SEND_MESSAGES: null
     }).then(() => {
-      message.channel.send('Lockdown lifted.');
+      message.channel.send({embed: {
+        color: 0x00ff00,
+        title: "Server Lockdown",
+        description: (`Server Lockdown has been lifted!\n`),
+        fields: [{
+          name: "Moderator:",
+          value: `${message.author.username} (${message.author})`,
+          inline: true
+        },{
+          name: "Lockdown timing:",
+          value: `${ms(ms(time), { long:true })}`,
+          inline: false
+        },{
+          name: "You are free to chat in the discord now, please avoid causing server lockdowns again!",
+          value: `~ Canadian Army`,
+          inline: false
+        }]
+          }})
       clearTimeout(client.lockit[message.channel.id]);
       delete client.lockit[message.channel.id];
     }).catch(error => {
@@ -19,12 +37,46 @@ exports.run = (client, message, args) => {
     message.channel.overwritePermissions(message.guild.id, {
       SEND_MESSAGES: false
     }).then(() => {
-      message.channel.send(`Channel locked down for ${ms(ms(time), { long:true })}`).then(() => {
+      message.channel.send({embed: {
+        color: 0xff0000,
+        title: "Server Lockdown",
+        description: (`Server Lockdown has been initiated\n`),
+        fields: [{
+          name: "Moderator:",
+          value: `${message.author.username} (${message.author})`,
+          inline: true
+        },{
+          name: "Lockdown timing:",
+          value: `${ms(ms(time), { long:true })}`,
+          inline: false
+        },{
+          name: `You are not allowed to speak in this channel for ${ms(ms(time), { long:true })}, please wait for the lockdown to be lifted.`,
+          value: `~ Canadian Army`,
+          inline: false
+        }]
+          }}).then(() => { //(`Channel locked down for ${ms(ms(time), { long:true })}`)
 
         client.lockit[message.channel.id] = setTimeout(() => {
           message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: null
-          }).then(message.channel.send('Lockdown lifted.')).catch(console.error);
+          }).then(message.channel.send({embed: {
+            color: 0x00ff00,
+            title: "Server Lockdown",
+            description: (`Server Lockdown has been lifted!\n`),
+            fields: [{
+              name: "Moderator:",
+              value: `${message.author.username} (${message.author})`,
+              inline: true
+            },{
+              name: "Lockdown timing:",
+              value: `${ms(ms(time), { long:true })}`,
+              inline: false
+            },{
+              name: "You are free to chat in the discord now, please avoid causing server lockdowns again!",
+              value: `~ Canadian Army`,
+              inline: false
+            }]
+              }})).catch(console.error);
           delete client.lockit[message.channel.id];
         }, ms(time));
 
@@ -33,6 +85,14 @@ exports.run = (client, message, args) => {
       });
     });
   }
+  const embed = new Discord.RichEmbed()
+  .setColor(0xff0000)
+  .setTimestamp()
+	.setTitle("Server Lockdown")
+  .setDescription(`\n`)
+  .addField("Moderator:",`${message.author.username} (${message.author})`,true)
+  .addField("Lockdown Timing:",`${ms(ms(time), { long:true })}`,false)
+  client.channels.get(modlog.id).send({embed}).catch(console.error);
 };
 exports.conf = {
   enabled: true,
