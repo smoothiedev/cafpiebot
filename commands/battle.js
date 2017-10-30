34caa32b2ade99ecce09e440a476b1e5b8116c08
smoothiedev <yeochenxi@yahoo.com>
2017-10-30 21:54:53 +0800

    Update battle.js

--- commands/battle.js
+++ commands/battle.js
@@ -1,9 +1,9 @@
-const Discord = require('discord.js');
 var sleep = require('system-sleep');
 exports.run = (client, message) => {
-    const modlog = client.channels.find('name', 'pie-log');
+    const modlog = client.channels.find('name', 'bot-commands');
     const user = message.mentions.users.first();
 if (message.mentions.users.size < 1) return message.reply('You must mention someone to battle them.').catch(console.error);
+if (message.channel.id == client.channels.get(modlog.id)) {
 message.channel.send({embed: {
   color: 0xff0000,
   title: ":fire: Battle Starting in 3!",
@@ -72,7 +72,7 @@ inline: true
       color: 0x00ff00,
       title: "Battle status: In progress..",
       description: (`:inbox_tray: ${message.author} did ${b} damage to ${user}`),
-      footer: {text = "4 more turns"},
+      footer: {text : "4 more turns"},
     fields: [{
     name: `${message.author}:`,
     value: `${c-b}/100`,
@@ -90,7 +90,7 @@ inline: true
       color: 0xff0000,
       title: "Battle status: In progress..",
       description: (`:outbox_tray: ${user} did ${wtf} damage to ${message.author}`),
-      footer: {text = "4 more turns"},
+      footer: {text : "4 more turns"},
     fields: [{
     name: `${message.author}:`,
     value: `${c}/100`,
@@ -107,7 +107,7 @@ var kys = Math.floor(Math.random() * 25) + 1
       color: 0x00ff00,
       title: "Battle status: In progress..",
       description: (`:inbox_tray: ${message.author} did ${kys} damage to ${user}`),
-      footer: {text = "3 more turns"},
+      footer: {text : "3 more turns"},
     fields: [{
     name: `${message.author}:`,
     value: `${e-kys}/100`,
@@ -125,7 +125,7 @@ var w0t = Math.floor(Math.random() * 25) + 1
       color: 0xff0000,
       title: "Battle status: In progress..",
       description: (`:outbox_tray: ${user} did ${w0t} damage to ${message.author}`),
-      footer: {text = "3 more turns"},
+      footer: {text : "3 more turns"},
     fields: [{
     name: `${message.author}:`,
     value: `${y}/100`,
@@ -142,7 +142,7 @@ var lao = Math.floor(Math.random() * 25) + 1
       color: 0x00ff00,
       title: "Battle status: In progress..",
       description: (`:inbox_tray: ${message.author} did ${lao} damage to ${user}`),
-      footer: {text = "2 more turns"},
+      footer: {text : "2 more turns"},
     fields: [{
     name: `${message.author}:`,
     value: `${y-lao}/100`,
@@ -160,7 +160,7 @@ var w0td = Math.floor(Math.random() * 25) + 1
       color: 0xff0000,
       title: "Battle status: In progress..",
       description: (`:outbox_tray: ${user} did ${w0td} damage to ${message.author}`),
-      footer: {text = "2 more turns"},
+      footer: {text : "2 more turns"},
     fields: [{
     name: `${message.author}:`,
     value: `${y-w0td}/100`,
@@ -178,7 +178,7 @@ var siala = Math.floor(Math.random() * 25) + 1
       color: 0x00ff00,
       title: "Battle status: In progress..",
       description: (`:inbox_tray: ${message.author} did ${siala} damage to ${user}`),
-      footer: {text = "1 more turn"},
+      footer: {text : "1 more turn"},
     fields: [{
     name: `${message.author}:`,
     value: `${ggg}/100`,
@@ -196,7 +196,7 @@ var fk = cb-siala
           color: 0xff0000,
           title: "Battle status: In progress..",
           description: (`:outbox_tray: ${user} did ${sialay} damage to ${message.author}`),
-          footer: {text = "Last turn"},
+          footer: {text : "Last turn"},
         fields: [{
         name: `${message.author}:`,
         value: `${ggg-sialay}/100`,
@@ -214,7 +214,7 @@ if (fk > ggg-sialay) {
           color: 0xffff00,
           title: "Battle status: Round ended!",
           description: (`:trophy: ${user} won the battle!`),
-          footer: {text = `Winner is ${user}`},
+          footer: {text : `Winner is ${user}`},
         fields: [{
         name: `${message.author}:`,
         value: `${ggg-sialay}/100`,
@@ -231,7 +231,7 @@ if (fk > ggg-sialay) {
       color: 0xff0000,
       title: "Battle status: Round ended!",
       description: (`:trophy: ${message.author} won the battle!`),
-      footer: {text = `Winner is ${message.author}`},
+      footer: {text : `Winner is ${message.author}`},
     fields: [{
     name: `${message.author}:`,
     value: `${ggg-sialay}/100`,
@@ -244,7 +244,21 @@ if (fk > ggg-sialay) {
     }})
     break
 }
-}};
+  }
+} else {
+  message.channel.send({embed: {
+    color: 0xff0000,
+    title: "Quiet Channel",
+    description: (``),
+  fields: [{
+  name: `${message.author}`,
+  value: `This is a quiet channel, please do not use ;battle here! Head over to #bot-commands instead.`,
+  inline: true
+  }],
+  }})
+}
+
+};
 
 exports.conf = {
 enabled: true,
