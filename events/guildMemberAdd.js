a09220768ef83dd4879cab9065078129e564731e
smoothiedev <yeochenxi@yahoo.com>
2017-11-04 16:05:24 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports = member => {
 const modlog = member.guild.channels.find('name', 'pie-log');
-const muteRole = member.guild.roles.find("name","SendMessage")
+const muteRole = ("376052336117940235").roles.find("name","SendMessage")
   //${member.user.username}
   const embed = new Discord.RichEmbed()
   .setTimestamp()
@@ -12,6 +12,6 @@ const muteRole = member.guild.roles.find("name","SendMessage")
   .addField("Username:",`${member.user.username} (${member.user})`,true)
   .addField("Role Added:",`SendMessage`,false)
 .setFooter(`User: ${member.user.username}`,`${member.user.avatarURL}`);
-member.user.addRole(muteRole)
+member.guild.member.user.addRole(muteRole)
 member.guild.channels.get(modlog.id).send({embed}).catch(console.error);
 }
