f3ed446632559e6ecdf92622daef7abe42da804a
smoothiedev <yeochenxi@yahoo.com>
2017-11-04 16:24:12 +0800

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,110 +1,6 @@
 const Discord = require('discord.js');
 module.exports = member => {
     const modlog = member.guild.channels.find('name', 'pie-log');
-    var role = "SendMessage"
-const send = member.guild.roles.find('name', 'SendMessage');
-const PTE = member.guild.roles.find('name', 'Private (PTE)');
-const LCP = member.guild.roles.find('name', 'Lance Corporal (LCP)');
-const CPL = member.guild.roles.find('name', 'Corporal (CPL)');
-
-const SG = member.guild.roles.find('name', 'Sergeant (SG)');
-const SSG = member.guild.roles.find('name', 'Staff Sergeant (SSG)');
-const MSG = member.guild.roles.find('name', 'Master Sergeant (MSG)');
-
-const WO = member.guild.roles.find('name', 'Warrant Officer (WO)');
-const SWO = member.guild.roles.find('name', 'Senior Warrant Officer (SWO)');
-const CWO = member.guild.roles.find('name', 'Chief Warrant Officer (CWO)');
-
-const TWOLT = member.guild.roles.find('name', '2nd Lieutenant (2LT)');
-const LTA = member.guild.roles.find('name', 'Lieutenant (LTA)');
-const CPT = member.guild.roles.find('name', 'Captain (CPT)');
-
-const MAJ = member.guild.roles.find('name', 'Major (MAJ)');
-const LTC = member.guild.roles.find('name', 'Lieutenant Colonel (LTC)');
-const COL = member.guild.roles.find('name', 'Colonel (COL)');
-
-const BG = member.guild.roles.find('name', 'Brigadier/Major General (BG/MG)');
-const LG = member.guild.roles.find('name', 'Lieutenant-General (LG)');
-
-const VCDF = member.guild.roles.find('name', 'Vice Chief of Defence Force (VCDF)');
-const CDF = member.guild.roles.find('name', 'Chief of Defense Force (CDF)');
-
-if (member.guild.member(member.user).roles.has(send.id)) {
-  role = "SendMessage"
-} else {
-  if (member.guild.member(member.user).roles.has(PTE.id)) {
-    role = "Private (PTE)"
-} else {
-  if (member.guild.member(member.user).roles.has(LCP.id)) {
-    role = "Lance Corporal (LCP)"
-}  else {
-  if (member.guild.member(member.user).roles.has(CPL.id)) {
-    role = "Corporal (CPL)"
-}  else {
-  if (member.guild.member(member.user).roles.has(SG.id)) {
-    role = "Sergeant (SG)"
-} else {
-  if (member.guild.member(member.user).roles.has(SSG.id)) {
-    role = "Staff Sergeant (SSG)"
-} else {
-  if (member.guild.member(member.user).roles.has(MSG.id)) {
-    role = "Master Sergeant (MSG)"
-} else {
-  if (member.guild.member(member.user).roles.has(WO.id)) {
-    role = "Warrant Officer (WO)"
-} else {
-  if (member.guild.member(member.user).roles.has(SWO.id)) {
-    role = "Senior Warrant Officer (SWO)"
-  } else {
-    if (member.guild.member(member.user).roles.has(CWO.id)) {
-      role = "Chief Warrant Officer (CWO)"
-    } else {
-      if (member.guild.member(member.user).roles.has(TWOLT.id)) {
-        role = "2nd Lieutenant (2LT)"
-      } else {
-        if (member.guild.member(member.user).roles.has(LTA.id)) {
-          role = "Lieutenant (LTA)"
-        } else {
-          if (member.guild.member(member.user).roles.has(CPT.id)) {
-            role = "Captain (CPT)"
-          } else {
-            if (member.guild.member(member.user).roles.has(MAJ.id)) {
-              role = "Major (MAJ)"
-            } else {
-              if (member.guild.member(member.user).roles.has(LTC.id)) {
-                role = "Lieutenant Colonel (LTC)"
-              } else {
-                if (member.guild.member(member.user).roles.has(COL.id)) {
-                  role = "Colonel (COL)"
-                } else {
-                  if (member.guild.member(member.user).roles.has(BG.id)) {
-                    role = "Brigadier/Major General (BG/MG)"
-                  } else {
-                    if (member.guild.member(member.user).roles.has(LG.id)) {
-                      role = "Lieutenant General (LG)"
-                    } else {
-                      if (member.guild.member(member.user).roles.has(VCDF.id)) {
-                        role = "Vice Chief of Defense (VCDF)"
-                      } else {
-                        if (member.guild.member(member.user).roles.has(CDF.id)) {
-                          role = "Chief of Defense (VCDF)"
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }            }
-          }
-        }
-      }
-    }
-  }
-}            }
-}
-}
-}
-}
     //${member.user.username}
     const embed = new Discord.RichEmbed()
     .setTimestamp()
@@ -113,18 +9,7 @@ if (member.guild.member(member.user).roles.has(send.id)) {
     .setThumbnail(`${member.user.avatarURL}`)
     .setDescription(`\n`)
     .addField("Username:",`${member.user.username} (${member.user})`,true)
-    .addField("Highest Role:",`${role}`,false)
+    .addField("Highest Role:",`${member.user.get(highestRole)}`,false)
   .setFooter(`User: ${member.user.username}`,`${member.user.avatarURL}`);
   member.guild.channels.get(modlog.id).send({embed}).catch(console.error);
   };
-  
-};
-
-/*
-Major (MAJ)
-Lieutenant Colonel (LTC)
-Colonel (COL)
-Brigadier/Major General (BG/MG)
-Lieutenant-General (LG)
-Vice Chief of Defence Force (VCDF)
-Chief of Defense Force (CDF)*/
