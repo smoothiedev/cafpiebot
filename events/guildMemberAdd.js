b4e3b2ae7f5b73cb707cd1a5d5c2068f7c2199a5
smoothiedev <yeochenxi@yahoo.com>
2017-11-04 12:31:21 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,16 @@
+const Discord = require('discord.js');
 module.exports = member => {
+const modlog = client.channels.find('name', 'pie-log');
   let guild = member.guild;
+  //${member.user.username}
+  const embed = new Discord.RichEmbed()
+  .setTimestamp()
+  .setColor(0xff0000)
+  .setTitle("User Joined")
+  .setThumbnail(`${member.user.avatarURL}`)
+  .setDescription(`\n`)
+  .addField("Username:",`${member.user.username} (${member.user})`,true)
+  .addField("Role Added:",`SendMessage`,false)
+.setFooter(`User: ${member.user.username}`,`${member.user.avatarURL}`);
+client.channels.get(modlog.id).send({embed}).catch(console.error);
 };
