472fd1f796445b287a6dbd495adb22f0826e50bd
smoothiedev <yeochenxi@yahoo.com>
2017-11-09 22:54:07 +0800

    Update ranks.js

--- commands/ranks.js
+++ commands/ranks.js
@@ -1,26 +1,35 @@
 exports.run = (client, message) => {
       message.channel.send({embed: {
-        title: "List of Canadian Army discord ranks.",
+        title: "List of Singapore Army discord ranks.",
 		  color: 3447003,
-  description: "This can be used with the ;rank command!",
+  description: "This can be used with the ;rank command! Please note that ranking people to the ranks not stated here will result in a demotion! You may not rank people higher than your role otherwise you will be punished!",
   fields: [{
-    name: "General Officers (BGen-Gen)",
+    name: "Brigadier General (BG)",
+    value: "Colonel (COL)"
+  },{
+    name: "Lieutenant Colonel (LTC)",
+    value: "Major (MAJ)"
+  },{
+    name: "Captain (CPT)",
+    value: "Lieutenant (LTA)"
+  },{
+    name: "2nd Lieutenant (2LT)",
+    value: "Chief Warrant Officer (CWO)"
+  },{
+    name: "Senior Warrant Officer (SWO)",
+    value: "Warrant Officer (WO)"
+  },{
+    name: "Master Sergeant (MSG)",
+    value: "Staff Sergeant (SSG)"
+  },{
+    name: "Sergeant (SG)",
+    value: "Corporal (CPL)"
+  },{
+    name: "Lance Corporal (LCP)",
+    value: "Private (PTE)"
+  },{
+    name: "Recruit (REC)",
     value: "Discord Admin"
-  },{
-    name: "Discord Moderator",
-    value: "Field Grade Officers (Maj-Col)"
-  },{
-    name: "Allied Representatives",
-    value: "RCAF"
-  },{
-    name: "CADTC",
-    value: "RCMP"
-  },{
-    name: "Company Grade Officers (2Lt-Cpt)",
-    value: "Staff Non-Commissioned Officers (WO-CWO)"
-  },{
-    name: "Non-Commissioned Officers (Cpl-Sgt)",
-    value: "Junior Enlisted (PTE B-PTE T)"
   }]
     }
 	});
