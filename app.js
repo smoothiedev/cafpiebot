a986b0f9e953c2dce55fc3bde14c7c42163365ff
smoothiedev <yeochenxi@yahoo.com>
2017-11-20 15:32:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const settings = require('./settings.json');
 const chalk = require('chalk');
 const fs = require('fs');
+var anti_spam = require("discord-anti-spam");
 const moment = require('moment');
 require('./util/eventLoader')(client);
 
@@ -24,7 +25,16 @@ fs.readdir('./commands/', (err, files) => {
     });
   });
 });
-
+antispam(bot, {
+  warnBuffer: 5, //Maximum amount of messages allowed to send in the interval time before getting warned.
+  maxBuffer: 10, // Maximum amount of messages allowed to send in the interval time before getting banned.
+  interval: 3000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warningMessage: "stop spamming or I'll whack your head off.", // Warning message send to the user indicating they are going to fast.
+  banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
+  maxDuplicatesWarning = 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan = 20 // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+      log(`Anti-Spam loaded!`);
+});
 client.reload = command => {
   return new Promise((resolve, reject) => {
     try {
