4b999224f5737928d88ac83d918ddfb9754094f3
smoothiedev <yeochenxi@yahoo.com>
2017-11-20 15:34:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const settings = require('./settings.json');
 const chalk = require('chalk');
 const fs = require('fs');
-var antispam = require("discord-anti-spam");
+var anti_spam = require("discord-anti-spam");
 const moment = require('moment');
 require('./util/eventLoader')(client);
 
@@ -31,9 +31,8 @@ antispam(bot, {
   interval: 3000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
   warningMessage: "stop spamming or I'll whack your head off.", // Warning message send to the user indicating they are going to fast.
   banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
-  maxDuplicatesWarning = 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
-  maxDuplicatesBan = 20 // Maximum amount of duplicate messages a user can send in a timespan before getting banned
-
+  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan: 20 // Maximum amount of duplicate messages a user can send in a timespan before getting banned
 });
 client.reload = command => {
   return new Promise((resolve, reject) => {
