a924d7b38761e40471b47a306b5dcec27d446bea
smoothiedev <yeochenxi@yahoo.com>
2017-11-27 13:43:49 +0800

    Update inactivity.js

--- commands/inactivity.js
+++ commands/inactivity.js
@@ -3,31 +3,6 @@ exports.run = (client, message) => {
   const modlog = client.channels.find('name', 'pie-log');
   const anno2 = client.channels.find('name', 'inactivity-notice');
 const guildmember = message.content.split(" ").join(" ").slice(12)
-const muteRole = client.guilds.get(message.guild.id).roles.find('name', `Inactive`);
-const say = args.slice(1).join(' ');
-if (say.length < 1) return message.reply('Please specify on the reason for going inactive.').catch(console.error);
-message.delete()
-message.guild.member(message.author).addRole(muteRole);
-client.channels.get(anno2.id).send({embed: {
-  color: 0xffff00,
-  title: `Inactivity Notice`,
-  description: ``,
-  fields: [{
-    name: "ROBLOX Username:",
-    value: `${guildmember}`
-  },{
-  name: "Message",
-  value: `${say}`
-  }]
-    }
-    })
-};
-
-const Discord = require('discord.js');
-exports.run = (client, message) => {
-  const modlog = client.channels.find('name', 'pie-log');
-  const anno2 = client.channels.find('name', 'inactivity-notice');
-const guildmember = message.content.split(" ").join(" ").slice(12)
 const say = args.slice(1).join(' ');
 if (say.length < 1) return message.reply('Please specify on what I have to say.').catch(console.error);
 message.delete()
@@ -62,18 +37,6 @@ name: "If you believe that this was an error, please use the command ;active.",
 value: "Otherwise, please use the command ;active after you are not inactive anymore to remove your inactive tag."
 }]
     }});
-exports.conf = {
-enabled: true,
-guildOnly: false,
-aliases: [],
-permLevel: 0
-};
-
-exports.help = {
-name: 'inactivity',
-description: 'Files an inactivity notice under the channel #inactivity-notice',
-usage: 'inactivity (ROBLOX USERNAME) message'
-};
 
 exports.conf = {
 enabled: true,
