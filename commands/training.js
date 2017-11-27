102483418f5bbe12ba922c0b997aa2b30c35aee6
smoothiedev <yeochenxi@yahoo.com>
2017-11-27 13:56:56 +0800

    Update training.js

--- commands/training.js
+++ commands/training.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, message) => {
   const modlog = client.channels.find('name', 'pie-log');
   const anno2 = client.channels.find('name', 'trainings');
-let say = message.content.split(" ").join(" ").slice(6)
+let say = message.content.split(" ").join(" ").slice(10)
 if (say.length < 1) return message.reply('Please specify on what I have to say.').catch(console.error);
 message.delete()
 
