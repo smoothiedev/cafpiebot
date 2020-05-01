d448b53d9f2430a9906f33aa7f1e2c45b708ca76
smoothiedev <yeochenxi@yahoo.com>
2020-05-02 02:13:01 +0800

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,16 +1,7 @@
 const Discord = require('discord.js');
 module.exports = member => {
-    const modlog = member.guild.channels.find('name', 'pie-log');
+    const modlog = member.guild.channels.find('name', '𝔭𝔦𝔢-𝔩𝔬𝔤');
     const guild = member.guild.member(member);
     //${member.user.username}
-    const embed = new Discord.RichEmbed()
-    .setTimestamp()
-    .setColor(0xff0000)
-    .setTitle("User Left")
-    .setThumbnail(`${member.user.avatarURL}`)
-    .setDescription(`\n`)
-    .addField("Username:",`${member.user.username} (${member.user})`,true)
-    .addField("Highest role:",`${guild.highestRole}`,true)
-  .setFooter(`User: ${member.user.username}`,`${member.user.avatarURL}`);
-  member.guild.channels.get(modlog.id).send({embed}).catch(console.error);
+
   };
