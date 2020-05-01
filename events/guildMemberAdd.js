020931f1b3cc0f7898948f2d588e2940a363ae46
smoothiedev <yeochenxi@yahoo.com>
2020-05-02 02:12:31 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports = member => {
-const modlog = member.guild.channels.find('name', 'pie-log');
-const muteRole = member.guild.roles.find("name","SendMessage")
+const modlog = member.guild.channels.find('name', '𝔭𝔦𝔢-𝔩𝔬𝔤');
+const muteRole = member.guild.roles.find("name","𝔖𝔢𝔫𝔡𝔐𝔢𝔰𝔰𝔞𝔤𝔢")
   //${member.user.username}
 member.guild.member(member.user).addRole(muteRole)
 
