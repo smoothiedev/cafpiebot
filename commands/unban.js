23616070b9d6f1270f00e8d6980260b5bb0d5639
smoothiedev <yeochenxi@yahoo.com>
2020-05-02 02:11:44 +0800

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
   client.unbanReason = reason;
   client.unbanAuth = message.author;
   const user = args[0];
-  const modlog = client.channels.find('name', 'mod-log');
+  const modlog = client.channels.find('name', '𝔪𝔬𝔡-𝔩𝔬𝔤');
   if (!modlog) return message.reply('I cannot find a mod-log channel');
   if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
   if (!user) return message.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
